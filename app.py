15f4a5048affebdc343642d21ec33bacd6a47a78
Jamie Davies <jamie@jagd.me>
2021-01-29 12:04:12 +0000

    Add random string to each code URL to prevent enumeration

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for
+from flask import abort, Flask, url_for
 from markupsafe import escape
 import os
 import yaml
@@ -15,15 +15,21 @@ def create_app():
         except yaml.YAMLError as e:
             print(e)
 
-    @app.route('/code/<int:code_number>')
-    def show_code(code_number):
+    @app.route('/<prefix>/<int:code_number>')
+    def show_code(prefix, code_number):
         number = int(escape(code_number))
-        code = config["codes"][number]["code"]
-        return f'Code {number} is {code}'
+        code = config["codes"].get(number)
+
+        if not code or prefix != code["prefix"]:
+            abort(404)
+
+        solution = code["code"]
+        return f'Code {number} is {solution}'
 
     with app.test_request_context():
-        for code in config["codes"]:
-            url = url_for('show_code', code_number=code)
-            print(f"Code {code}\t{url}")
+        for code_number in config["codes"]:
+            prefix = config["codes"][code_number]["prefix"]
+            url = url_for('show_code', code_number=code_number, prefix=prefix)
+            print(f"Code {code_number}\t{url}")
 
     return app
